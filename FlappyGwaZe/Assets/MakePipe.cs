1823b83cd0c6a03a05d465ef2edb0020fa56ef34
YoungSi4 <dksdudgns01@gmail.com>
2023-09-13 15:40:43 +0900

    파이프 생성
    높이가 랜덤하게 생성되고 왼쪽으로 움직인다

---
+++ FlappyGwaZe/Assets/MakePipe.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakePipe : MonoBehaviour
+{
+    public GameObject Pipe;
+    public float timediff;
+    float timer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > timediff)
+        {
+            GameObject newpipe = Instantiate(Pipe);
+            newpipe.transform.position = new Vector3(0, Random.Range(0f, 5f), 0);
+            timer= 0;
+        }
+    }
+}
